8dd41d9cef1bc54eb6aef1f42bac34986e504299
ajaybhatta987 <ajaybhatta987@gmail.com>
2023-08-10 20:03:37 +0545

    setting env-var

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cchjc6en!l5dz@@%e)#blnqk28e9xg*)(&15(qk!yze=t^ehr2"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
