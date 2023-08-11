fbb4f20221ece60abe60149af8e8eb591ec60e79
ajaybhatta987 <ajaybhatta987@gmail.com>
2023-08-11 13:24:19 +0545

    added secretkey

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
-# import os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cchjc6en!l5dz@@%e)#blnqk28e9xg*)(&15(qk!yze=t^ehr2"
-# os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
